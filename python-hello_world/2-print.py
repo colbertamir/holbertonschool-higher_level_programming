4d7cf52c0e298d907db1e7365352a5f87644ea1c
colbertamir <colbertamir@gmail.com>
2023-09-06 17:30:46 -0700

    Added new line?

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 # This will print "Programming is like building a multilingual puzzle followed by a new line
-print("\"Programming is like building a multilingual puzzle" )
+print("\"Programming is like building a multilingual puzzle")
+
