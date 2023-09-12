6953672c0618bc8a5826d789bcbd84870aaea617
colbertamir <colbertamir@gmail.com>
2023-09-12 16:58:23 -0700

    small edit to 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            print(matrix[i][j], end=" ")
+            print(f"{matrix[i][j]}", end=" ")
         print()
 
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
