f4ff7810c33b34dc7eaed55874d9faa9080ffc69
colbertamir <colbertamir@gmail.com>
2023-09-12 16:25:00 -0700

    Made edits to 5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 # Can you C me now?
 # Removes all c and C characters from a string
 def no_c(my_string):
-      copy = [e for e in my_string if e != 'c' and x != 'C']
+      copy = [e for e in my_string if e != 'c' and e != 'C']
       return("".join(copy))
 
