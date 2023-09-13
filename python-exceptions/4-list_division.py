9b358b3ecc04dcdcca6d8e339243ca01c61f056b
colbertamir <colbertamir@gmail.com>
2023-09-12 19:48:20 -0700

    edit

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 # 4-list_division.py
 # Divides element by element 2 lists
-def list_division(my_list_1, my_list_2, list_length):
-    Args:
-        my_list_1 (list): The first list.
-        my_list_2 (list): The second list.
-        list_length (int): The number of elements to divide.
-
-        Returns:
+def list_division(my_list_1, my_list_2, list_length):i
+            
             new_list = []
             for i in range(0, list_length):
                 try:
