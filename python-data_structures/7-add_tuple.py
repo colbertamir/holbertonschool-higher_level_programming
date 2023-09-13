fc38a2df4aeca2c4aeb806b620b2ac8d156015a7
colbertamir <colbertamir@gmail.com>
2023-09-12 17:19:45 -0700

    test

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,10 +3,4 @@
 # Add two tuples
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_sum = tuple_a + tuple_b
-
-    for i in range(len(tuple_a)):
-        tuple_sum.append(tuple_a[i])
-    else:
-        tuple_sum[i] += tuple_b[i]
-
         return tuple_sum
