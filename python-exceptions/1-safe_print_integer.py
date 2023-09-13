93d7c4e428373bcc75189d9cd5a2f1d4d087230f
colbertamir <colbertamir@gmail.com>
2023-09-12 19:27:35 -0700

    edit

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,14 +2,9 @@
 # 1-safe_print_integer.py
 # Prints an integer with "{:d}".format().
 def safe_print_integer(value):
-    Args:
-        value (int): The integer to print.
-
-        Returns:
-             If a TypeError or ValueError occurs - False.
-             Otherwise - True.
-        try:
-            print("{:d}".format(value))
-            return (True)
-        except (TypeError, ValueError):
-            return (False)
+    
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
