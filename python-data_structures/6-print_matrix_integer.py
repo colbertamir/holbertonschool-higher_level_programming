ed528e78420db72e8ee2da75ca82ec72b97c6587
colbertamir <colbertamir@gmail.com>
2023-09-13 09:03:22 -0700

    edit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,11 @@
 # 6-print_matrix_integer.py
 # Prints a matrix
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for col in row:
-            print(f"{col:d}", end=" ")
-        print()
+    
+    if not matrix:
+        return
 
-    print_matrix_integer(matrix)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            print(str.format("{0:d}", matrix[i][j]), end= " ")
+        print()
