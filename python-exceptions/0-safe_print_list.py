6303361071ec66e6a43a34df6311cd2282fe48ea
colbertamir <colbertamir@gmail.com>
2023-09-12 19:22:12 -0700

    edit

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,16 +3,12 @@
 # Prints 'x' elements of a list
 def safe_print_list(my_list=[], x=0):
     
-    if not my_list:
-        return 0
-
-    ret = 0
+    real_len = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
-            ret += 1
+            print(my_list[i], end="")
+            real_len += 1
         except IndexError:
-            print("The list has only {} elements.".format(len(my_list)))
             break
-        print("")
-        return (ret)
+        print()
+        return real_len
