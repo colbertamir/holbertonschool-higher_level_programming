5f2f9014a44ea382c793f82bedb5fe4e11a83660
colbertamir <colbertamir@gmail.com>
2023-09-12 18:20:34 -0700

    edit

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,6 +2,9 @@
 # 2-uniq_add.py
 # Adds all unique integers in a list (only once for each integer)
 def uniq_add(my_list=[]):
+    if not my_list:
+        return 0
+
     uniq_list = set(my_list)
     num = 0
 
