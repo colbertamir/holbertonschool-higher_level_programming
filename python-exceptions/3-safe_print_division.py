4b989d1990faec03fc43b12b7dc8b1c6dc1e891a
colbertamir <colbertamir@gmail.com>
2023-09-12 19:45:49 -0700

    edit

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,10 +2,14 @@
 # 3-safe_print_division.py
 # Divides 2 integers and prints the result
 def safe_print_division(a, b):
+    
+    if not isinstance(a, int) or not isinstance(b, int):
+        raise TypeError("Both arguments must be integers.")
+
     try:
         div = a / b
-    except (TypeError, ZeroDivisionError):
+    except ZeroDivisionError:
         div = None
-     finally:
-         print("Inside result: {}".format(div))
-        return (div)
+
+        print("Inside result: {}".format(div))
+         return (div)
