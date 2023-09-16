a5676fed0efae62a8f6db6f9dc80a589e408ff91
colbertamir <colbertamir@gmail.com>
2023-09-15 18:16:41 -0700

    edit

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -2,4 +2,4 @@
 # 11-multiply_list_map.py
 # Returns a list with all values multiplied by a number without using any loops
 def multiply_list_map(my_list=[], number=0):
-    return (list(map((lambda i: i * number), my_list)))
+    return (list(map((lambda v: v * number), my_list)))
