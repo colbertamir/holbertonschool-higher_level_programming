4d2236bc0b4995e1c1be61c00a9e057fc6dc900e
colbertamir <colbertamir@gmail.com>
2023-09-17 16:38:14 -0700

    edit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,5 +8,5 @@ class Square:
         self.__size = size
     if not isinstance(size, int)
     raise: TypeError(size must be an integer)
-    if < 0
+    elif < 0
     raise: ValueError(size must be >= 0)
