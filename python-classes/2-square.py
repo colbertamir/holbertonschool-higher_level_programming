fe6207101a582e18f929e3159cce43488770ada5
colbertamir <colbertamir@gmail.com>
2023-09-17 16:59:34 -0700

    edit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,9 +4,11 @@
 
 class Square:
     """ Instantiation Size and Raised exceptions """
+
     def __init__(self, size=0):
-        self.__size = size
-    if not isinstance(size, int)
-    raise: TypeError(size must be an integer)
-    elif < 0
-    raise: ValueError(size must be >= 0)
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
