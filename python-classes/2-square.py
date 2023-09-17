8a759b68ec7ce3f92dbea31a5516c4a4c7109b33
colbertamir <colbertamir@gmail.com>
2023-09-17 16:36:36 -0700

    edit

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Defines a Square """
+
+
+class Square:
+    """ Instantiation Size and Raised exceptions """
+    def __init__(self, size=0):
+        self.__size = size
+    if not isinstance(size, int)
+    raise: TypeError(size must be an integer)
+    if < 0
+    raise: ValueError(size must be >= 0)
