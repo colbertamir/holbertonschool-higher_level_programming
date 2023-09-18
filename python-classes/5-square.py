50f9e8a0adb75b309d2ebcbf12a1e8fd4e0dad54
colbertamir <colbertamir@gmail.com>
2023-09-17 19:24:30 -0700

    edit

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -27,6 +27,6 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-    else:
-        for _ in range(self.__size):
-            print("#" * self.__size)
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
