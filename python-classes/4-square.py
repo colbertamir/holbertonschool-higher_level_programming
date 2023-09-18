e55c6a151343cb9e974ed53a5a8cd8f654cbc65b
colbertamir <colbertamir@gmail.com>
2023-09-17 18:36:25 -0700

    edit

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,13 +4,13 @@
 
 class Square:
     """ Instantiation Size and Raised exceptions; Now getting and setting """
-
+    
     def __init__(self, size=0):
         self.size = size
-
+    @property
     def size(self):
         return self.__size
-
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
