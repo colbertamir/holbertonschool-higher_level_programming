7433ce13fcefa6173b9ab923888ed053a81181e7
colbertamir <colbertamir@gmail.com>
2023-09-20 08:49:10 -0700

    edit

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_square(size):
-    
 
     """Prints a square of '#' characters.
 
@@ -12,6 +11,7 @@ def print_square(size):
         ValueError: If `size` is less than zero.
 
     """
+
 
 if not isinstance(size, int):
     raise TypeError("size must be an integer")
