b0e596bc32515c1ec890c8d4f06c6714cc3d6ca0
colbertamir <colbertamir@gmail.com>
2023-09-20 09:09:31 -0700

    edit

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -7,24 +7,32 @@ def print_square(size):
         size (int): The height and width of the square.
 
     Raises:
-        TypeError: If `size` is not an integer.
-        ValueError: If `size` is less than zero.
+        TypeError: If 'size' is not an integer.
+        ValueError: If 'size' is less than zero.
 
     """
 
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
 
-if not isinstance(size, int):
-    raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
 
-if size < 0:
-    raise ValueError("size must be >= 0")
+    if size > 0:
+        for _ in range(size):
+            print("#" * size)
 
-if size > 0:
-    for _ in range(size):
-        print("#" * size)
 
 if __name__ == "__main__":
-    print_square(1)
     print_square(4)
+    print("")
     print_square(10)
+    print("")
     print_square(0)
+    print("")
+    print_square(1)
+    print("")
+    try:
+        print_square(-1)
+    except Exception as e:
+        print(e)
