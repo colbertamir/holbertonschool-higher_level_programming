9a272695ff9827c11d4c95e6deca8a9d0f3a1944
colbertamir <colbertamir@gmail.com>
2023-09-19 21:37:59 -0700

    edit

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-# 2-safe_print_list_integers.py
-# Prints the first 'x' elements of a list and only integers
 def safe_print_list_integers(my_list=[], x=0):
+    count = 0
 
-    a = 0
-    for i in range(0, x):
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            a += 1
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
         except (ValueError, TypeError):
             continue
-        print("")
-        return a
-if __name__ == "2-main":
+
+    print()
+    return count
+
+
+if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
     nb_print = safe_print_list_integers(my_list, 2)
     print("nb_print: {:d}".format(nb_print))
