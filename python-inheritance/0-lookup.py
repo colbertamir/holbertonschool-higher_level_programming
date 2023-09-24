130fb2e9ae6a072d0ca867099ff87546d6a63eca
colbertamir <colbertamir@gmail.com>
2023-09-24 12:15:31 -0700

    got rid of docstring

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 
 def lookup(obj):
-    """
-    Get a list of public attributes and methods of an object.
-
-    Args:
-        obj: The object being inspected
-
-    Returns:
-        A list of public attributes/method names
-    """
 
     attributes_and_methods = dir(obj)
 
