943b41994a581e0e366dcfb8bb5d2ef531df6846
colbertamir <colbertamir@gmail.com>
2023-09-24 12:26:57 -0700

    0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
+""" Defines and obj attribute lookup function """
+
 
 def lookup(obj):
 
-    attributes_and_methods = dir(obj)
 
-    return attributes_and_methods
-
-
-class MyClass1(object):
-    pass
-
-
-class MyClass2(object):
-    my_attr1 = 3
-
-    def my_meth(self):
-        pass
-
-
-print(lookup(MyClass1))
-print(lookup(MyClass2))
-print(lookup(int))
+    """ Returns objects attribute's """
+    return (dir(obj))
