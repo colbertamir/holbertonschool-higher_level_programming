148fa07e5326706b34096dea0d315497215d8285
colbertamir <colbertamir@gmail.com>
2023-09-25 18:30:08 -0700

    10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines Square a subclass of Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializes square.
+
+        Args:
+            size (int): The size of the square
+        """
+        super().__init__(size, size)
+        self.__size = size
