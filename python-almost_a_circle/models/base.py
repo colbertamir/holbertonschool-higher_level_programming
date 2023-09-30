bda1daa76476003a7e7784581bc875f5b2380438
colbertamir <colbertamir1516@gmail.com>
2023-09-30 10:30:17 -0700

    Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,23 @@
-#!/usr/bin/python3
-""" Defining Base class """
-
-
 class Base:
-     __nb_objects = 0
+    """Base class for managing id attributes."""
 
+    __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Initializes an instance of the Base class.
+
+        Args:
+            id (int, optional): Is an integer identifier.
+            If provided, it is assigned to
+            the 'id' attribute. If 'None' is passed, a new 'id'
+            is created by incrementing the '__nb_objects' counter.
+
+        Returns:
+            None
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
