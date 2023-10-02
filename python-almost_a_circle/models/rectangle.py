3f5cc0d6e8439d54e10cb03515fbadc6cfc846dd
colbertamir <colbertamir@gmail.com>
2023-10-01 22:11:49 -0700

    edit to 8 & 9

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -108,13 +108,46 @@ def __str__(self):
 
 
 def update(self, *args, **kwargs):
-    """Update Rectangle with keyword arguments.
+    """Updates the Rectangle.
 
         Args:
-            *args: Not used
+            *args (int): Attribute values.
+                1st arg represents id attribute
+                2nd arg represents width attribute
+                3rd arg represent height attribute
+                4th arg represents x attribute
+                5th arg represents y attribute
             **kwargs (dict): Key-value pairs of attributes.
     """
-    attributes = ["id", "width", "height", "x", "y"]
-    for key, value in kwargs.items():
-        if key in attributes:
-            setattr(self, key, value)
+    if args and len(args) != 0:
+        a = 0
+        for arg in args:
+            if a == 0:
+                if arg is None:
+                    self.__init__(self.width, self.height, self.x, self.y)
+                else:
+                    self.id = arg
+            elif a == 1:
+                self.width = arg
+            elif a == 2:
+                self.height = arg
+            elif a == 3:
+                self.x = arg
+            elif a == 4:
+                self.y = arg
+                a += 1
+    elif kwargs and len(kwargs) != 0:
+        for k, v in kwargs.items():
+            if k == "id":
+                if v is None:
+                    self.__init__(self.width, self.height, self.x, self.y)
+                else:
+                    self.id = v
+            elif k == "width":
+                self.width = v
+            elif k == "height":
+                self.height = v
+            elif k == "x":
+                self.x = v
+            elif k == "y":
+                self.y = v
