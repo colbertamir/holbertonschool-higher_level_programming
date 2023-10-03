6a21a4d59be4f799ce168d29ec938e48ac3e44b6
colbertamir <colbertamir@gmail.com>
2023-10-02 19:32:53 -0700

    update base again

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -28,11 +28,12 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Return the JSON serialization of a list of dicts.
+        """Return the JSON version of a list of dictionaries.
 
         Args:
             list_dictionaries (list): A list of dictionaries.
+
+        Returns:
+            str: JSON representation of list_dictionaries.
         """
-        if list_dictionaries is None or list_dictionaries == []:
-            return "[]"
-        return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries) if list_dictionaries else "[]"
