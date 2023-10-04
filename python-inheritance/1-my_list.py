58a7925c9c43a290c6340b80ab3f59466b1cdbf3
colbertamir <colbertamir@gmail.com>
2023-10-04 16:05:18 -0700

    11

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-"""Defines an inherited class called MyList"""
+"""Defines inherited class MyList."""
 
 
 class MyList(list):
-    """ Inherited class Mylist from built-in class list """
+    """Inherited class MyList from built-in class list."""
 
     def print_sorted(self):
-        """ Prints a list sorted in ascending order """
+        """Prints the list sorted in ascending order."""
         print(sorted(self))
+
+
+if __name__ == "__main__":
+    my_list = MyList()
+    my_list.append(4)
+    my_list.append(2)
+    my_list.append(7)
+    my_list.print_sorted()
