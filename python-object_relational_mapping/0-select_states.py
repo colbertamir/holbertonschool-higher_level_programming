915ed0504f5db4c813e6ce70d1287238d18681ca
colbertamir <colbertamir1516@gmail.com>
2023-10-21 10:56:08 -0700

    Edit

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,44 @@
 #!/usr/bin/python3
 """Lists all states from the database 'hbtn_0e_0_usa'"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check if there are 3 command-line arguments
     if len(sys.argv) != 4:
         sys.exit("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
 
-    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    # Extract the command-line arguments
+    mysql_username, mysql_password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    # Connect to SQL server running on localhost at port 3306
-    db = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        passwd=password,
-        db=database_name,
-        port=3306
-    )
+    try:
+        # Connect to SQL server running on localhost at port 3306
+        db = MySQLdb.connect(
+            host="localhost",
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name,
+            port=3306
+        )
 
-    cur = db.cursor()
-    
-    # Execute the SQL query to select all states
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+        # Create a cursor
+        cur = db.cursor()
 
-    rows = cur.fetchall()
+        # Execute the SQL query to select all states
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    for row in rows:
-        print(row)
+        # Fetch all rows
+        rows = cur.fetchall()
 
-    cur.close()
-    db.close()
+        # Display the results
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+    finally:
+        # Close the cursor and database connection
+        if 'cur' in locals():
+            cur.close()
+        if 'db' in locals():
+            db.close()
