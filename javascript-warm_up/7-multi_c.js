4f77928e587c5abfbdf824344e5d2674190362dc
colbertamir <colbertamir1516@gmail.com>
2023-11-27 13:40:12 -0800

    7-multi_c.js

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const numOccurrences = Number(process.argv[2]);
+
+if (isNaN(numOccurrences)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < numOccurrences) {
+    console.log('C is fun');
+    i++;
+  }
+}
