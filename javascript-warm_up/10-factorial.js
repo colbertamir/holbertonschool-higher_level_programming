43a6079aabf84f7b2b2835d858391f069a9a0b2b
colbertamir <colbertamir1516@gmail.com>
2023-11-28 09:53:10 -0800

    10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+
+  if (n < 0) {
+    return -1;
+  }
+
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(Number(process.argv[2])));
